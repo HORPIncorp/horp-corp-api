21226526fd15828142f43782a1bd2713b6a484ba
Olivia Pace <albanypace@gmail.com>
2025-10-23 17:37:18 -0400

    Catalog + Get Item by ID

---
+++ HORPIncorp.Api/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Jet.Piranha.Domain.Catalog;
+
+namespace Jet.Piranha.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>
+            {
+                new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
+                new Item ("Shorts", "Ohio State Shorts", "Nike", 44.99m)
+            };
+            return Ok(items);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            
+            return Ok(item);
+        }
+
+    }
+}
