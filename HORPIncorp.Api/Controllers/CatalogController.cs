83756522679e2ac4dcce46978b2921d5790bfa17
Pranava Koyyada <pranava.koyyada@gmail.com>
2025-10-28 16:48:15 -0400

    post/put/delete

--- HORPIncorp.Api/Controllers/CatalogController.cs
+++ HORPIncorp.Api/Controllers/CatalogController.cs
@@ -26,5 +26,33 @@ namespace Jet.Piranha.Api.Controllers
             return Ok(item);
         }
 
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
     }
 }
