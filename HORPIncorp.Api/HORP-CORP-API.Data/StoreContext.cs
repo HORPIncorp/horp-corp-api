2692f8368401b7d2a178c2d3e0a87f5ddec2fed6
ZeroMercy <hassandirie008@gmail.com>
2025-10-31 16:02:28 -0400

    Fix DbContextOptions typo in StoreContext constructor

---
+++ HORPIncorp.Api/HORP-CORP-API.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using HORP_CORP_API.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HORP_CORP_API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {}
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
